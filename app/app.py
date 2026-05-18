16aa5eefd85882b48936170204cc68c84d1a3488
Ishani Arora <ishani.arora2005@gmail.com>
2026-05-18 15:46:33 +0530

    added reqs and warning

--- app/app.py
+++ app/app.py
@@ -92,5 +92,6 @@ if uploaded_file is not None:
 st.warning(
     "This system is for AI-assisted analysis only "
     "and not a replacement for professional "
-    "medical diagnosis."
+    "medical diagnosis. Please"
+    "reach out to a trusted health physician."
 )
